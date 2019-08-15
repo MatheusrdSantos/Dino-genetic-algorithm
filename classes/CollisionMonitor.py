b22e24cc86e6075daddfbfcd96f66db75e40ecb3
Matheus Ricardo dos Santos <matheus.r.d.s10@hotmail.com>
2019-08-14 22:45:25 -0300

    Created CollisionMonitor class

---
+++ classes/CollisionMonitor.py
@@ -0,0 +1,12 @@
+"""
+Author: Matheus Santos
+Description: this class monitors the game elements. If a collision is detected, this class must notify the game controller.
+"""
+
+class ColisionMonitor:
+    def __init__(self, master, canvas, elements = [], blocks = []):
+        self.master = master
+        self.canvas = canvas
+        # elements that can't crash with blocks
+        self.elements = elements
+        self.blocks = blocks
--- classes/GameController.py
+++ classes/GameController.py
@@ -4,7 +4,7 @@ Description: this class manage the entire game state.
 """
 from tkinter import *
 from PIL import Image, ImageTk
-
+from classes.CollisionMonitor import ColisionMonitor
 
 class GameController:
     def __init__(self, mode):
@@ -12,10 +12,12 @@ class GameController:
         self.mode = mode
         self.master = Tk()
         self.canvas = Canvas(self.master, width=800, height=800, bg='#eee')
+        self.colisionMonitor = ColisionMonitor(self.master, self.canvas)
     def run(self):
         if(self.mode == "game"):
             self.canvas.pack()
             self.prepareGame()
             mainloop()
+    # create game elements
     def prepareGame(self):
         pass
