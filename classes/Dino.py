d698b6605c6674e0463b9af33df807c0e273b754
Matheus Ricardo dos Santos <matheus.r.d.s10@hotmail.com>
2019-08-17 13:38:25 -0300

    Implement Dino jumping

--- classes/Dino.py
+++ classes/Dino.py
@@ -11,9 +11,32 @@ class Dino:
         self.master = master
         self.canvas = canvas
         self.jump_height = jump_height
+        self.moving = False
+        self.distance = 0
+        self.jumping_id = None
         #TODO: change image names
         img_pil = Image.open("./assets/dino.png")
         self.image = ImageTk.PhotoImage(img_pil)
         self.id = self.canvas.create_image(100, 650, image=self.image, anchor=NW)
-        #self.master.bind('<Up>', self.jump_call)
-        #self.master.bind('<Down>', self.down)
+        self.master.bind('<Up>', self.jump_call)
+        self.master.bind('<Down>', self.down)
+
+    def jump_call(self, event):
+        if(not self.moving):
+            self.moving = True
+            self.jump(event)
+    def jump(self, event):
+        if(self.distance<self.jump_height):
+            self.distance+=1
+            self.canvas.move(self.id, 0, -1)
+            self.jumping_id = self.canvas.after(3, self.jump, event)
+        elif(self.distance>=self.jump_height and self.distance<self.jump_height*2):
+            self.distance+=1
+            self.canvas.move(self.id, 0, 1)
+            self.jumping_id = self.canvas.after(3, self.jump, event)
+        else:
+            self.distance = 0
+            self.moving = False
+
+    def down(self, event):
+        pass
