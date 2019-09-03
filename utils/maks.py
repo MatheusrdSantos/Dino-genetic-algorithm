c33b99cf1727378ec2a1943ae6d76f5b8b9a25b1
Matheus Santos <matheus.r.d.s10@hotmail.com>
2019-09-03 11:34:40 -0300

    Implement print imagen on terminal from mask

---
+++ utils/maks.py
@@ -0,0 +1,22 @@
+from PIL import Image
+import numpy as np
+from colr import color
+img_pil = Image.open("./assets/dino.png")
+#a = np.asarray(img_pil)
+pix_val = list(img_pil.getdata())
+
+def printImage(image_vector, dimensions=[28,28], bg=False):
+    for index, pixel in enumerate(image_vector, start=1):
+        if bg:
+            back = (pixel[3], pixel[3], pixel[3])
+        else:
+            back = (255-pixel[3], 255-pixel[3], 255-pixel[3])
+        if(index%dimensions[0]==0):
+            print(color(' ', fore=back, back=back))
+        else:
+            print(color(' ', fore=back, back=back), end="")
+
+printImage(pix_val, [48, 50])
+
+def reduceImageTo(image, originalDimension=[48, 50], finalDimension=[10, 10]):
+    pass
