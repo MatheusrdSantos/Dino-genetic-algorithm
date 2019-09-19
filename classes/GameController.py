ff72e8b984cbfa643b9d1e2aab6d34fd77f2f858
Matheus Ricardo dos Santos <matheus.r.d.s10@hotmail.com>
2019-09-18 21:39:00 -0300

    Add ground image

--- classes/GameController.py
+++ classes/GameController.py
@@ -2,7 +2,7 @@
 Author: Matheus Santos
 Description: this class manage the entire game state.
 """
-from tkinter import Canvas, Tk, mainloop
+from tkinter import Canvas, Tk, mainloop, NW
 from PIL import Image, ImageTk
 from classes.CollisionMonitor import ColisionMonitor
 from classes.Dino import Dino
@@ -23,6 +23,14 @@ class GameController:
         self.colisionMonitor = None
         self.obstacleGenerator = None
         self.master.bind('<r>', self.restart)
+        self.imgs_pil_ground = [
+            Image.open("./assets/ground.png"),
+            Image.open("./assets/ground-1.png")]
+        self.ground = ImageTk.PhotoImage(self.imgs_pil_ground[0])
+        self.ground_1 = ImageTk.PhotoImage(self.imgs_pil_ground[1])
+        # display image on canvas
+        self.ground_id = self.canvas.create_image(0, 700, image=self.ground, anchor=NW)
+        self.ground_id_1 = self.canvas.create_image(400, 700, image=self.ground_1, anchor=NW)
     def run(self):
         if(self.mode == "game"):
             self.canvas.pack()
